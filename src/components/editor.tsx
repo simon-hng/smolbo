38ccb5cfed5f6e6361032817e8dd6e5727128973
Simon Huang <simon.huang@tum.de>
2023-05-19 12:13:59 +0200

    Add basic styles and editing

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -9,8 +9,10 @@ export const Editor = ({ card }: EditorProps) => {
   return (
     <div>
       <h2>Front</h2>
+      <textarea className="bg-slate-900">{card.front}</textarea>
 
       <h2>Back</h2>
+      <textarea className="bg-slate-900">{card.back}</textarea>
     </div>
   );
 };
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,5 +3,9 @@ interface LayoutProps {
 }
 
 export const Layout = ({ children }: LayoutProps) => {
-  return <div className="min-h-screen bg-slate-900 text-white">{children}</div>;
+  return (
+    <div className="min-h-screen bg-slate-900 text-white">
+      <div className="p-12">{children}</div>
+    </div>
+  );
 };
