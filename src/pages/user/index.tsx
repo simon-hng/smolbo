ffa3272fbcfb02700583ce129c5a79a5f36eb14a
Simon Huang <simon.huang@tum.de>
2023-05-19 04:33:39 +0200

    Add authorization

---
+++ src/pages/user/index.tsx
@@ -0,0 +1,30 @@
+import { type NextPage } from "next";
+import { useSession } from "next-auth/react";
+import * as Avatar from "@radix-ui/react-avatar";
+
+const User: NextPage = () => {
+  const { data: session } = useSession();
+
+  if (!session?.user) {
+    return <p>You&apos;re not signed in </p>;
+  }
+
+  return (
+    <div>
+      <Avatar.Root className="">
+        {session.user.image && (
+          <Avatar.Image
+            className="rounded-full"
+            src={session.user.image}
+            alt={session.user.name ?? "Your profile picture"}
+          />
+        )}
+        <Avatar.Fallback>{session.user.name}</Avatar.Fallback>
+      </Avatar.Root>
+
+      <p>Welcome {session.user.name}</p>
+    </div>
+  );
+};
+
+export default User;
