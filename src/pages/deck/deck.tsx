cebad5e20504503f4a1afa159ac55b634ffd727c
Simon Huang <simon.huang@tum.de>
2023-05-19 11:09:23 +0200

    Add deck page

---
+++ src/pages/deck/deck.tsx
@@ -0,0 +1,11 @@
+export const Deck = () => {
+  return (
+    <div>
+      <h1>Deck name</h1>
+      <p>Description</p>
+
+      <h2>All cards</h2>
+    </div>
+  );
+};
+
---
+++ src/pages/deck/index.tsx
@@ -0,0 +1,3 @@
+import { Deck } from "./deck";
+
+export default Deck;
