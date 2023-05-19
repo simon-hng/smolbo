c6df593bd14c63b241401638c874de426dabcb44
Simon Huang <simon.huang@tum.de>
2023-05-19 11:45:28 +0200

    Remove editor implementation

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,29 +1,16 @@
 import { type Card } from "@prisma/client";
 import { type PartialBy } from "~/utils/ts-utils";
-import { Editable, useEditor } from "@wysimark/react";
 
 interface EditorProps {
   card: PartialBy<Card, "id">;
 }
 
 export const Editor = ({ card }: EditorProps) => {
-  const frontEditor = useEditor({
-    initialMarkdown: card.front,
-    height: 240,
-  });
-
-  const backEditor = useEditor({
-    initialMarkdown: card.back,
-    height: 240,
-  });
-
   return (
     <div>
       <h2>Front</h2>
-      <Editable editor={frontEditor} />
 
       <h2>Back</h2>
-      <Editable editor={backEditor} />
     </div>
   );
 };
