590414d920258b507e12ffdc7d60fef8d20cc40a
Simon Huang <simon.huang@tum.de>
2023-05-20 15:57:00 +0200

    Add useravatar component

---
+++ src/components/userAvatar/userAvatar.tsx
@@ -0,0 +1,28 @@
+import * as Avatar from "@radix-ui/react-avatar";
+import { useSession } from "next-auth/react";
+
+interface UserAvatarProps {
+  className?: string;
+}
+
+export const UserAvatar = ({ className }: UserAvatarProps) => {
+  const { data: session } = useSession();
+
+  return (
+    <Avatar.Root className={className}>
+      {session?.user.image && (
+        <Avatar.Image
+          className="rounded-full"
+          src={session.user.image}
+          alt={session.user.name ?? "Your profile picture"}
+        />
+      )}
+
+      {session?.user.name ? (
+        <Avatar.Fallback>{session.user.name}</Avatar.Fallback>
+      ) : (
+        <Avatar.Fallback>?</Avatar.Fallback>
+      )}
+    </Avatar.Root>
+  );
+};
