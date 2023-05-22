e697bc71b09ee29648446272c4b11875356da04a
Simon Huang <simon.huang@tum.de>
2023-05-22 23:12:36 +0200

    Change border color to slate

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -7,7 +7,7 @@ import { UserAvatar } from "../userAvatar";
 export const Navigation: React.FC = () => {
   return (
     <NavigationMenu.Root>
-      <NavigationMenu.List className="flex items-center border-b-2 border-b-white px-12 py-4">
+      <NavigationMenu.List className="flex items-center border-b-2 border-b-slate-500 px-12 py-4">
         <NavigationMenu.Item>
           <Link href="/decks">Decks</Link>
         </NavigationMenu.Item>
@@ -17,7 +17,7 @@ export const Navigation: React.FC = () => {
             <UserAvatar className="w-8" />
             <CaretDownIcon className="ml-2" aria-hidden />
           </NavigationMenu.Trigger>
-          <NavigationMenu.Content className="absolute mt-2 rounded border-2 border-white bg-slate-900 px-2">
+          <NavigationMenu.Content className="absolute mt-2 rounded border-2 border-slate-500 bg-slate-900 px-2">
             <ul className="space-y-2 py-2">
               <li>
                 <Link href="/user">Account</Link>
--- src/pages/decks/[id].tsx
+++ src/pages/decks/[id].tsx
@@ -12,7 +12,7 @@ const CardsCarousel = ({ cards }: CardsCarouselProps) => {
     <div className="flex space-x-4 overflow-x-auto scroll-smooth py-8">
       {cards &&
         cards.map((card) => (
-          <div key={card.id} className="border-2 border-white p-8">
+          <div key={card.id} className="border-2 border-slate-500 p-8">
             <h1>{card.front}</h1>
             <p>{card.back}</p>
           </div>
