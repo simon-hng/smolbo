297744df69a028c46dcf0b112d2c91c8408fdbeb
Simon Huang <simon.huang@tum.de>
2023-05-22 19:59:22 +0200

    Format navigation and change link to next router

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -7,9 +7,9 @@ import { UserAvatar } from "../userAvatar";
 export const Navigation: React.FC = () => {
   return (
     <NavigationMenu.Root>
-      <NavigationMenu.List className="flex items-center px-12 py-4 border-b-white border-b-2">
+      <NavigationMenu.List className="flex items-center border-b-2 border-b-white px-12 py-4">
         <NavigationMenu.Item>
-          <NavigationMenu.Link href="/decks">Decks</NavigationMenu.Link>
+          <Link href="/decks">Decks</Link>
         </NavigationMenu.Item>
 
         <NavigationMenu.Item className="ml-auto">
@@ -17,7 +17,7 @@ export const Navigation: React.FC = () => {
             <UserAvatar className="w-8" />
             <CaretDownIcon className="ml-2" aria-hidden />
           </NavigationMenu.Trigger>
-          <NavigationMenu.Content className="absolute bg-slate-900 mt-2 px-2 rounded border-2 border-white">
+          <NavigationMenu.Content className="absolute mt-2 rounded border-2 border-white bg-slate-900 px-2">
             <ul className="space-y-2 py-2">
               <li>
                 <Link href="user">Account</Link>
