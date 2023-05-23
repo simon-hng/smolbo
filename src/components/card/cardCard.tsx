3ae51a7ddb575cb0eb7ed7bb742401478797dcd1
Simon Huang <simon.huang@tum.de>
2023-05-24 00:28:36 +0200

    Add card card styles

--- src/components/card/cardCard.tsx
+++ src/components/card/cardCard.tsx
@@ -1,20 +1,51 @@
 import { type Card } from "@prisma/client";
+import { useState } from "react";
 import { Remark } from "react-remark";
+import * as Separator from "@radix-ui/react-separator";
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+
+const CardCardDropdownMenu = () => {
+  return (
+    <DropdownMenu.Root>
+      <div className="mb-4 flex items-center justify-end">
+        <DropdownMenu.Trigger>
+          <HamburgerMenuIcon />
+        </DropdownMenu.Trigger>
+      </div>
+
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content>
+          <DropdownMenu.Item>Edit card</DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+  );
+};
 
 interface CardCardProps {
   card: Card;
 }
 
 export const CardCard = ({ card }: CardCardProps) => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div key={card.id} className="card h-full w-96">
-      <details className="prose prose-invert cursor-pointer">
-        <summary className="list-none">
+      <CardCardDropdownMenu />
+      <div className="prose prose-invert">
+        <div className="cursor-pointer" onClick={() => setOpen(true)}>
           <Remark>{card.front}</Remark>
-        </summary>
+        </div>
 
-        <Remark>{card.back}</Remark>
-      </details>
+        {open && (
+          <div>
+            <Separator.Root className="h-[1px] bg-white" />
+
+            <Remark>{card.back}</Remark>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
