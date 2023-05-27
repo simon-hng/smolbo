8e4bd45c08c3869261b9e3665b591ff22e7ab479
Simon Huang <simon.huang@tum.de>
2023-05-28 01:44:03 +0200

    Add card swiping

--- src/pages/decks/[id].tsx
+++ src/pages/decks/[id].tsx
@@ -1,4 +1,5 @@
-import { PanInfo, motion } from "framer-motion";
+import type { Card } from "@prisma/client";
+import { type PanInfo, motion, useAnimation } from "framer-motion";
 import { type NextPage } from "next";
 import { useRouter } from "next/router";
 import { useState } from "react";
@@ -8,43 +9,58 @@ import { api } from "~/utils/api";
 const DecksViewPage: NextPage = () => {
   const { query } = useRouter();
 
+  const [card, setCard] = useState<Card>();
   const learningSetQuery = api.deck.getLearningSet.useQuery(
     query.id as string,
-    { enabled: !!query.id }
+    { enabled: !!query.id, onSuccess: (data) => setCard(data[0]) }
   );
+  const cards = learningSetQuery.data;
+  const [cardIndex, setCardIndex] = useState(1);
 
-  const cards = learningSetQuery.data ?? [];
-  const [cardIndex, setCardIndex] = useState(0);
-  const [currentCard, setCurrentCard] = useState(cards[1]);
-
-  const getNextCard = () => {
-    setCardIndex(cardIndex + 1);
-    setCurrentCard(cards[cardIndex]);
-  };
+  const controls = useAnimation();
 
   const dragEndHandler = (_event: Event, info: PanInfo) => {
-    const threshhold = 500;
-    if (info.offset.x > threshhold) {
-      getNextCard();
-    }
+    const swipeThreshhold = window.innerWidth / 2;
+    if (Math.abs(info.offset.x) < swipeThreshhold || !cards) return;
 
-    if (info.offset.x < threshhold) {
-      getNextCard();
-    }
+    setCard(cards[cardIndex]);
+    setCardIndex(cardIndex + 1);
+
+    void controls
+      .start({
+        x: -1.25 * window.innerWidth,
+        scale: 1.2,
+      })
+      .then(() => {
+        controls.set({ x: window.innerWidth });
+      })
+      .then(() => {
+        void controls.start({
+          x: 0,
+          scale: 1,
+        });
+      });
   };
+
+  if (learningSetQuery.isLoading) {
+    return <h1>Loading</h1>;
+  }
+
+  if (!card) {
+    return <h1>No more cards</h1>;
+  }
 
   return (
     <div className="overflow-visible py-8">
-      {currentCard && (
-        <motion.div
-          drag="x"
-          dragSnapToOrigin
-          whileDrag={{ scale: 1.2 }}
-          onDragEnd={dragEndHandler}
-        >
-          <CardCard card={currentCard} />
-        </motion.div>
-      )}
+      <motion.div
+        drag="x"
+        dragSnapToOrigin
+        whileDrag={{ scale: 1.2 }}
+        onDragEnd={dragEndHandler}
+        animate={controls}
+      >
+        <CardCard card={card} />
+      </motion.div>
     </div>
   );
 };
