75d02817afdc29e711c62d8f1154cf52fad97cd6
Simon Huang <simon.huang@tum.de>
2023-05-28 01:46:24 +0200

    Remove example trpc router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
 import { cardRouter } from "./routers/card";
 import { deckRouter } from "./routers/deck";
 
@@ -9,7 +8,6 @@ import { deckRouter } from "./routers/deck";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   card: cardRouter,
   deck: deckRouter,
 });
