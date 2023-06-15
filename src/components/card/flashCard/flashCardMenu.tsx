3ace088a6d237d7712261cd64e1e2be294080fbd
Simon Huang <simon.huang@tum.de>
2023-06-15 12:32:13 +0200

    Update flashcard dropdown menu style

--- src/components/card/flashCard/flashCardMenu.tsx
+++ src/components/card/flashCard/flashCardMenu.tsx
@@ -1,12 +1,8 @@
 import type { Card } from "@prisma/client";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
-import {
-  HamburgerMenuIcon,
-  Pencil1Icon,
-  TrashIcon,
-} from "@radix-ui/react-icons";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import toast from "react-hot-toast";
-import { Button } from "~/components/ui/button";
+import { Card as CardComponent } from "~/components/ui/card";
 import { api } from "~/utils/api";
 
 interface Props {
@@ -36,27 +32,26 @@ export const CardMenu = ({ card, isEdit, setIsEdit }: Props) => {
       </div>
 
       <DropdownMenu.Portal>
-        <DropdownMenu.Content className="mt-2 space-y-2 text-white">
-          <DropdownMenu.Item asChild>
-            <Button
-              color="primary"
-              fullWidth
-              onClick={() => setIsEdit(!isEdit)}
-            >
-              <Pencil1Icon className="mr-2" aria-hidden />
-              Edit
-            </Button>
-          </DropdownMenu.Item>
-          <DropdownMenu.Item asChild>
-            <Button
-              color="red"
-              fullWidth
-              onClick={() => void deleteCardMutation.mutate(card.id)}
-            >
-              <TrashIcon className="mr-2" aria-hidden />
-              Delete
-            </Button>
-          </DropdownMenu.Item>
+        <DropdownMenu.Content asChild>
+          <CardComponent
+            color="glass"
+            padding="none"
+            className="mt-4 flex flex-col divide-y divide-slate-500 text-white"
+          >
+            <DropdownMenu.Item asChild>
+              <button className="px-4 py-2" onClick={() => setIsEdit(!isEdit)}>
+                Edit
+              </button>
+            </DropdownMenu.Item>
+            <DropdownMenu.Item asChild>
+              <button
+                className="bg-red-900 px-4 py-2"
+                onClick={() => void deleteCardMutation.mutate(card.id)}
+              >
+                Delete
+              </button>
+            </DropdownMenu.Item>
+          </CardComponent>
         </DropdownMenu.Content>
       </DropdownMenu.Portal>
     </DropdownMenu.Root>
