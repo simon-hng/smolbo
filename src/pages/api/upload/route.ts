3a5d7d8d3c2aae6eef9a1380f9f870d2fe892b79
Simon Huang <simon.huang@tum.de>
2023-06-24 01:16:09 +0200

    Add embeddings of pdfs

--- src/pages/api/upload/route.ts
+++ src/pages/api/upload/route.ts
@@ -1,6 +1,12 @@
-import formidable from "formidable";
+import formidable, { type Fields, type Files } from "formidable";
 import { type IncomingMessage } from "http";
 import type { NextApiResponse } from "next/types";
+import { PDFLoader } from "langchain/document_loaders";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+import { PineconeClient } from "@pinecone-database/pinecone";
+import { env } from "~/env.mjs";
 
 export const config = {
   api: {
@@ -8,12 +14,50 @@ export const config = {
   },
 };
 
-export default function handler(req: IncomingMessage, res: NextApiResponse) {
+const createEmbeddings = async (filePath: string) => {
+  const pinecone = new PineconeClient();
+  await pinecone.init({
+    environment: env.PINECONE_ENVIRONMENT,
+    apiKey: env.PINECONE_API_KEY,
+  });
+
+  const loader = new PDFLoader(filePath);
+  const rawData = await loader.load();
+
+  console.log(rawData);
+
+  const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    chunkOverlap: 200,
+  });
+
+  const chunkedData = await textSplitter.splitDocuments(rawData);
+
+  const embeddings = new OpenAIEmbeddings();
+  const pineconeIndex = pinecone.Index(env.PINECONE_INDEX);
+
+  await PineconeStore.fromDocuments(chunkedData, embeddings, {
+    pineconeIndex,
+    namespace: "flashcards",
+    textKey: "text",
+  });
+};
+
+export default async function handler(
+  req: IncomingMessage,
+  res: NextApiResponse
+) {
   const form = formidable({});
 
-  form.parse(req, (err, fields, files) => {
-    console.log(err, fields, files);
-  });
+  const formParse = form.parse(req) as unknown as Promise<[Fields, Files, any]>;
+  const [_fields, files] = await formParse;
+
+  for (const file of Object.values(files)) {
+    if (Array.isArray(file)) {
+      const filePath: string = file.at(0)?.filepath ?? "";
+      await createEmbeddings(filePath);
+    }
+  }
 
   res.status(200).json({ success: "true" });
 }
