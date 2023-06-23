78a074f0d8dae788300b89b52561c6b649b26133
Simon Huang <simon.huang@tum.de>
2023-06-24 01:53:46 +0200

    Add api route for chatting with embeddings

--- src/server/api/routers/deck.ts
+++ src/server/api/routers/deck.ts
@@ -1,5 +1,44 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+import { ConversationalRetrievalQAChain } from "langchain/chains";
+import type { VectorOperationsApi } from "@pinecone-database/pinecone/dist/pinecone-generated-ts-fetch";
+import { OpenAI } from "langchain";
+
+const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:`;
+
+const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
+If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
+If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+
+{context}
+
+Question: {question}
+Helpful answer in markdown:`;
+
+export const makeChain = (vectorstore: PineconeStore) => {
+  const model = new OpenAI({
+    temperature: 0,
+    modelName: "gpt-3.5-turbo",
+  });
+
+  const chain = ConversationalRetrievalQAChain.fromLLM(
+    model,
+    vectorstore.asRetriever(),
+    {
+      qaTemplate: QA_PROMPT,
+      questionGeneratorTemplate: CONDENSE_PROMPT,
+      returnSourceDocuments: true, //The number of source documents returned is 4 by default
+    }
+  );
+  return chain;
+};
 
 export const deckRouter = createTRPCRouter({
   create: protectedProcedure
@@ -86,5 +125,35 @@ export const deckRouter = createTRPCRouter({
           id: input,
         },
       });
+    }),
+
+  chatWithSlides: protectedProcedure
+    .input(
+      z.object({
+        question: z.string(),
+        history: z.array(z.string()),
+        deckId: z.string(),
+      })
+    )
+    .query(async ({ input }) => {
+      const pineconeIndex = input.deckId;
+
+      const vectorStore = await PineconeStore.fromExistingIndex(
+        new OpenAIEmbeddings({}),
+        {
+          pineconeIndex: pineconeIndex as unknown as VectorOperationsApi,
+          textKey: "text",
+          namespace: "flashcards",
+        }
+      );
+
+      const chain = makeChain(vectorStore);
+
+      const response = await chain.call({
+        question: input.question.trim().replaceAll("\n", ""),
+        chat_history: input.history,
+      });
+
+      return response;
     }),
 });
