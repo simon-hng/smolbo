9764205989aff30bc907eab5cb1ecfd8fcd32aac
Simon Huang <simon.huang@tum.de>
2023-06-27 16:38:17 +0200

    Refactor upload of multiple pdfs

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,7 +1,7 @@
-import formidable, { type Fields, type Files } from "formidable";
+import formidable, { type Fields, type Files, type File } from "formidable";
 import { type IncomingMessage } from "http";
 import type { NextApiResponse } from "next/types";
-import { PDFLoader } from "langchain/document_loaders";
+import { DirectoryLoader, PDFLoader } from "langchain/document_loaders";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { PineconeStore } from "langchain/vectorstores/pinecone";
@@ -12,33 +12,6 @@ export const config = {
   api: {
     bodyParser: false,
   },
-};
-
-const createEmbeddings = async (filePath: string, deckId: string) => {
-  const pinecone = new PineconeClient();
-  await pinecone.init({
-    environment: env.PINECONE_ENVIRONMENT,
-    apiKey: env.PINECONE_API_KEY,
-  });
-
-  const loader = new PDFLoader(filePath);
-  const rawData = await loader.load();
-
-  const textSplitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 1000,
-    chunkOverlap: 200,
-  });
-
-  const chunkedData = await textSplitter.splitDocuments(rawData);
-
-  const embeddings = new OpenAIEmbeddings();
-  const pineconeIndex = pinecone.Index(env.PINECONE_INDEX);
-
-  await PineconeStore.fromDocuments(chunkedData, embeddings, {
-    pineconeIndex,
-    namespace: deckId,
-    textKey: "text",
-  });
 };
 
 export default async function handler(
@@ -56,11 +29,32 @@ export default async function handler(
     return
   }
 
-  // TODO: Refactor so that embeddings are not created one file at a time
-  for (const file of Object.values(data)) {
-    if (Array.isArray(file)) return;
-    createEmbeddings(file.filepath, deckId)
-  }
+  const pinecone = new PineconeClient();
+  await pinecone.init({
+    environment: env.PINECONE_ENVIRONMENT,
+    apiKey: env.PINECONE_API_KEY,
+  });
+
+  const files = await Promise.all(
+    Object.values(data).filter((file): file is File => !Array.isArray(file))
+      .map((file) => new PDFLoader(file.filepath).load())
+  ).then(files => files.flat())
+
+  const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    chunkOverlap: 200,
+  });
+
+  const chunkedData = await textSplitter.splitDocuments(files);
+
+  const embeddings = new OpenAIEmbeddings();
+  const pineconeIndex = pinecone.Index(env.PINECONE_INDEX);
+
+  await PineconeStore.fromDocuments(chunkedData, embeddings, {
+    pineconeIndex,
+    namespace: deckId,
+    textKey: "text",
+  });
 
   res.status(200).json({ success: "true" });
 }
