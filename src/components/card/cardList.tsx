386bf2b4fd2f33a0b8f915ee5d7222209b73bf8a
Simon Huang <simon.huang@tum.de>
2023-07-03 02:25:11 +0200

    Improve card list styles

--- src/components/card/cardList.tsx
+++ src/components/card/cardList.tsx
@@ -9,7 +9,11 @@ export const CardList = ({ cards }: Props) => {
     <div className="flex min-h-[50vh] space-x-4 overflow-auto">
       {cards &&
         cards.map((card) => (
-          <FlashCard key={card.id} card={card} className="w-64 shrink-0" />
+          <FlashCard
+            key={card.id}
+            card={card}
+            className="w-3/4 shrink-0 md:w-1/3"
+          />
         ))}
     </div>
   );
