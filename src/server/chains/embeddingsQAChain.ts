02acec289e8f78a00e67aeaa69e8a861e08972a4
Simon Huang <simon.huang@tum.de>
2023-07-19 21:05:09 +0200

    feat: improve chat prompt for latex

--- src/server/chains/embeddingsQAChain.ts
+++ src/server/chains/embeddingsQAChain.ts
@@ -12,7 +12,13 @@ Standalone question:`;
 const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
-Add any page numbers that you sourced or slide numbers if no page numbers are available.
+If asking for equations, add them in LaTeX. If using LaTeX, escape block LaTeX like this:
+
+$$
+block LaTeX
+$$
+
+Inline LaTeX is escaped like this: $inline LaTeX$
 
 {context}
 
